299eb299af768f7ea7ca7a949e2c2491866e4ad6
souravkcoder <souravk.coder@gmail.com>
2023-01-30 01:40:01 +0600

    bfs and dfs of clrs added

--- Graph/bfs_clrs.cpp
+++ Graph/bfs_clrs.cpp
@@ -1,48 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t=0;
-
-void dfs_visit(vector<vector<int>>&vertex, vector<int>adj[],int u){
-
-    t++;
-    vertex[u][1]=t;
-    vertex[u][0]=1;
-    for(auto v:adj[u]){
-        if(vertex[v][0]==0){
-            vertex[v][2]=u;
-            dfs_visit(vertex,adj,v);
-        }
-
-    }
-    vertex[u][0]=2;
-    t++;
-    vertex[u][4]=t;
-
-
-
-}
-
-vector<vector<int>>dfs_traversal(int n,vector<int>adj[]){
+vector<vector<int>> bfs_traversal(int n, vector<int>adj[],int s ){
 
     vector<vector<int>>vertex(n+1);
     for(int i=1; i<=n; i++)
     {
-        vector<int>temp(5);
+        vector<int>temp(4);
         temp[0]=0;          //color
         temp[1]=INT_MAX;    //distance
         temp[2]=-1;         //parent
         temp[3]=i;          //vertex
-        temp[4]=INT_MAX;
         vertex[i]=temp;
     }
 
-    for(int i=n;i>=1;i--){
-        if(vertex[i][0]==0){
-            dfs_visit(vertex,adj,i);
+    vertex[s][0]=1;
+    vertex[s][1]=0;
+    vertex[s][2]=-1;
+
+    queue<int>q;
+    q.push(s);
+
+    while(!q.empty()){
+        int u=q.front();
+        q.pop();
+        for(auto v: adj[u]){
+            if(vertex[v][0]==0){
+               vertex[v][0]=1;
+               vertex[v][1]=vertex[u][1]+1;
+               vertex[v][2]=u;
+               q.push(v);
+            }
         }
+        vertex[u][0]=2;
     }
     return vertex;
+
 }
 
 int main() {
@@ -58,15 +51,13 @@ int main() {
             //Comment the next line for directed graph
             adj[v].push_back(u);
         }
-        vector<vector<int>>ans=dfs_traversal(n,adj);
-        cout<<"DFS Traversal is : ";
+        vector<vector<int>>ans=bfs_traversal(n,adj,1);
+        cout<<"BFS Traversal is : ";
         for(auto it: ans){
           //  cout<<it[0]<<" "<<it[1]<<" "<<it[2]<<endl;
           for (auto x: it) cout<<x<<" ";
           cout<<endl;
         }
-
-
 
 
 
